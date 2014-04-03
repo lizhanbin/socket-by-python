eb99ab56f9672572c0f4bc3ba241ac36b0a16e34
lizhanbin <lizhanbin0214@gmail.com>
2014-04-03 19:08:43 +0800

    Create client.py
    client

---
+++ client.py
@@ -0,0 +1,23 @@
+import sys
+from socket import *
+
+serverHost = 'localhost'
+serverPort = 5007
+
+message = ['hello network world']
+
+if len(sys.argv) > 1:
+    serverHost = sys.argv[1]
+if len(sys.argv) > 2:
+    message = sys.argv[2]
+
+sockobj = socket(AF_INET, SOCK_STREAM)
+sockobj.connect((serverHost, serverPort))
+
+for line in message:
+    sockobj.send(line)
+
+data = sockobj.recv(1024)
+print 'Client received:', repr(data)
+
+sockobj.close()
